5e6fb068c72f6653f1b8216bce8e7c20d227ba9b
Alexey Dvoryaninov <lexxzone@gmail.com>
2017-10-21 23:25:15 +0300

    Point + Triangle + 3.4. Максимум из трех чисел [#190]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,3 +3,9 @@ public class Max {
     public int max (int first, int second) {
     return first > second ? first : second;
     }
+    
+    public int max (int first, int second, int third) {
+    return Math.max(first, Math.max(second, third));
+    }
+    
+}
