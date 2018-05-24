d2c86770dbc04b2124c088d34532215533bc4ded
LexxZone <lexxzone@gmail.com>
2018-05-24 10:26:19 +0300

    Added convert method

--- Collections-lite/src/main/java/ru/job4j/lite/list/ConvertList2Array.java
+++ Collections-lite/src/main/java/ru/job4j/lite/list/ConvertList2Array.java
@@ -1,8 +1,9 @@
 package ru.job4j.lite.list;
 /**
- * Created by Alex Dvoryaninov on 06.05.18
+ * Created by Alex Dvoryaninov on 24.05.18
  */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
@@ -10,8 +11,6 @@ public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
 
         int cells = list.size() / rows + (list.size() % rows == 0 ? 0 : 1);
-
-        //System.out.println(list.get(list.size() - 1) + " - last element \n" + list.size() + " - размер");
 
         int[][] array = new int[rows][cells];
 
@@ -32,6 +31,33 @@ public class ConvertList2Array {
 
         return array;
     }
+
+    public List<Integer> convert(List<int[]> list) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int[] singleArray : list) {
+
+            for (int i = 0; i < singleArray.length; i++) {
+
+                result.add(singleArray[i]);
+
+            }
+        }
+
+        return result;
+    }
+
+/*    public static void main(String[] args) {
+
+        ConvertList2Array CL2A = new ConvertList2Array();
+
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        List<Integer> result = CL2A.convert(list);
+
+    }*/
 
 }
 
