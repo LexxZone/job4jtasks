8ec90becd0b71ead8efb89f94665ce4397c7f5a4
Alexey Dvoryaninov <lexxzone@gmail.com>
2018-07-08 22:44:03 +0300

    Added new methods of sorting and wrote Tests

--- Collections-lite/src/main/java/ru/job4j/lite/sort/SortUser.java
+++ Collections-lite/src/main/java/ru/job4j/lite/sort/SortUser.java
@@ -1,16 +1,13 @@
 package ru.job4j.lite.sort;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Created by Alexey Dvoryaninov on 23.06.2018 - (lexxzone@gmail.com)
  */
 public class SortUser {
     /**
-     *
+     * Sorting with Comparable
      * Method returns TreeSet of users, sorted by the age (min to max). If there are two or more users with the same
      * age they are sorted by the name
      *
@@ -25,19 +22,53 @@ public class SortUser {
         return users;
     }
 
-    /*public static void main(String[] args) {
+    /**
+     * Sorting by the Name length
+     * @param userList2 incoming list
+     * @return users - sorted list
+     */
+    public List<User> sortNameLength (List<User> userList2) {
+        Comparator<User> userComparator = new Comparator<User>() {
+            public int compare(User one, User two) {
+                return (one.getName().length() - two.getName().length());
+            }
+        };
+        Collections.sort(userList2, userComparator);
+        return userList2;
+    }
+
+    public List<User> sortByAllFields (List<User> userList3) {
+
+        Comparator<User> userComparator = new Comparator<User>() {
+            public int compare(User one, User two) {
+
+                return (one.getName().compareTo(two.getName())) == 0 ? one.getAge() - two.getAge():one.getName().compareTo(two.getName());
+
+            }
+        };
+
+        Collections.sort(userList3, userComparator);
+
+        return userList3;
+    }
+
+    public static void main(String[] args) {
 
         List<User> list = new ArrayList<>();
         User chris = new User("Chris", 27);
         User bella = new User("Bella", 22);
+        User bella2 = new User("Bella", 17);
         User john = new User("John", 37);
         User sven = new User("Sven", 29);
+        User chris2 = new User("Chris", 22);
         User alex = new User("Alex", 30);
         User jess = new User("Jess", 29);
         list.add(chris);
         list.add(bella);
+        list.add(bella2);
         list.add(john);
         list.add(sven);
+        list.add(chris2);
         list.add(alex);
         list.add(jess);
 
@@ -49,7 +80,11 @@ public class SortUser {
 
         System.out.println("\n\n\n==========\n\n\n");
 
-        User [] result = new SortUser().sort(list).toArray(new User[0]);
+        User [] result = new SortUser().sortByAllFields(list).toArray(new User[0]);
+
+        //User [] result = new SortUser().sortNameLength(list).toArray(new User[0]);
+        //User [] result = new SortUser().sort(list).toArray(new User[0]);
+
 
         for (User usr: result
              ) {
@@ -58,5 +93,5 @@ public class SortUser {
         }
 
 
-    }*/
+    }
 }
--- Collections-lite/src/test/java/ru/job4j/lite/sort/SortUserTest.java
+++ Collections-lite/src/test/java/ru/job4j/lite/sort/SortUserTest.java
@@ -8,6 +8,9 @@ import static org.junit.Assert.assertThat;
 
 /**
  * Created by Alexey Dvoryaninov on 23.06.2018 - (lexxzone@gmail.com)
+ *
+ * This test is testing different methods of sorting
+ *
  */
 public class SortUserTest {
     /**
@@ -36,5 +39,57 @@ public class SortUserTest {
         User [] result = new SortUser().sort(list).toArray(new User[0]);
         User [] expected = new User [] {bella, chris, jess, sven, alex, john};
         assertThat(result, is(expected));
+
     }
+
+    /**
+     *  This Test is checking sorting of dynamic array of objects <User> by the name length
+     */
+    @Test
+    public void whenSortedByNameLength() {     // ---------
+        List<User> list = new ArrayList<>();
+        User chris = new User("Christian", 27);
+        User bella = new User("Bella", 22);
+        User john = new User("Johnny", 37);
+        User sven = new User("Sven", 29);
+        User alex = new User("Alexandr", 30);
+        User jess = new User("Jessica", 29);
+        list.add(chris);
+        list.add(bella);
+        list.add(john);
+        list.add(sven);
+        list.add(alex);
+        list.add(jess);
+        User [] result = new SortUser().sortNameLength(list).toArray(new User[0]);
+        User [] expected = new User [] {sven, bella, john, jess, alex, chris};
+        assertThat(result, is(expected));
+    }
+
+    /**
+     *  This Test is checking sorting of dynamic array of objects <User> by the name and after by the age
+     */
+    @Test
+    public void whenSortedByAllFields() {    // --------
+        List<User> list = new ArrayList<>();
+        User chris = new User("Chris", 27);
+        User bella = new User("Bella", 22);
+        User john = new User("John", 37);
+        User jess2 = new User("Jess", 40);
+        User sven = new User("Sven", 29);
+        User chris2 = new User("Chris", 20);
+        User alex = new User("Alex", 30);
+        User jess = new User("Jess", 29);
+        list.add(chris);
+        list.add(bella);
+        list.add(john);
+        list.add(jess2);
+        list.add(sven);
+        list.add(chris2);
+        list.add(alex);
+        list.add(jess);
+        User [] result = new SortUser().sortByAllFields(list).toArray(new User[0]);
+        User [] expected = new User [] {alex, bella, chris2, chris, jess, jess2, john, sven};
+        assertThat(result, is(expected));
+    }
+
 }
