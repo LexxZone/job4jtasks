d7cdcbf912e5ee7339f65bbabf76f9eaffa616fa
Alexey Dvoryaninov <lexxzone@gmail.com>
2018-07-14 22:37:04 +0300

    Added package-info.java file

---
+++ Collections-lite/src/main/java/ru/job4j/lite/sort/package-info.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Alexey Dvoryaninov (lexxzone@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ *
+ * This package have different sorting methods
+ *
+ */
+package ru.job4j.lite.sort;
