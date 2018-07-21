8b71b53130794fbe39d4c356856114a8cde970ae
Alexey Dvoryaninov <lexxzone@gmail.com>
2018-07-21 20:00:17 +0300

    Fixed some issues

--- chapter_002/src/main/java/ru/job4j/polymorphism/Paint.java
+++ chapter_002/src/main/java/ru/job4j/polymorphism/Paint.java
@@ -5,18 +5,12 @@ public class Paint {
     
     
     public static void main (String[] args) {
-        
         Paint paint = new Paint();
-        
         paint.draw (new Square());
         paint.draw (new Triangle());
-    
     }
+
     public void draw(Shape shape){
-        
         System.out.println(shape.draw());
-        
     }
-
-
 }
--- chapter_002/src/main/java/ru/job4j/polymorphism/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/polymorphism/Triangle.java
@@ -2,13 +2,13 @@ package ru.job4j.polymorphism;
 
 public class Triangle implements Shape {
     @Override
-	public String draw() {
-    StringBuilder pic = new StringBuilder();
-    pic.append("   +   \n");
-    pic.append("  +++  \n");
-    pic.append(" +++++ \n");
-    pic.append("+++++++\n");
-    return pic.toString();
+    public String draw() {
+        StringBuilder pic = new StringBuilder();
+        pic.append("   +   \n");
+        pic.append("  +++  \n");
+        pic.append(" +++++ \n");
+        pic.append("+++++++");
+        return pic.toString();
     }
 
 }
